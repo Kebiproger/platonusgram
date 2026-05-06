2b342a74dd44df3f24bcdcd1899d417e2fb7e5b0
Kebiproger <qanagats1mple007@gmail.com>
2026-05-06 23:30:12 +0500

    fixed bugs

--- web_server.py
+++ web_server.py
@@ -2,7 +2,7 @@ from aiohttp import web
 from crypto import encrypt_password
 from db_api import save_user, init_db
 import os
-from bot_handler import get_main_kb
+from keyboards import get_main_kb
 
 # Временное хранилище токенов (в идеале использовать Redis, но для начала хватит словаря)
 # Формат: { "token_string": telegram_id }
