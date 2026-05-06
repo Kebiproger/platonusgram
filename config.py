be586df21e18cd1cecfd64204c3f7325034a362e
Kebiproger <qanagats1mple007@gmail.com>
2026-05-06 20:15:08 +0500

    few changes

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ import os
 
 # Подгружаем данные из .env
 load_dotenv()
-# LOGIN = os.getenv("PLATONUS_LOGIN")
-# PASSWORD = os.getenv("PLATONUS_PASSWORD")
 TELEGRAM_TOKEN=os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID=os.getenv("TELEGRAM_CHAT_ID")
 URL=os.getenv("SERVER_URL")
